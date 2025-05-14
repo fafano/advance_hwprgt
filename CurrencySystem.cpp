47350a32fe1ecbabad1af059053a5478f6248c4b
fafa <fatemehnourani278@gmail.com>
2025-05-14 18:36:34 +0330

    currency secend try (Adding operators)

--- CurrencySystem.cpp
+++ CurrencySystem.cpp
@@ -2,61 +2,142 @@
 
 using namespace std;
 
-
-float Currency::changetocu(curr cu , float sum){
-          if(cu==curr::usd){
-            currency= cu;
-            base = sum;
-            getcu();
-           return base;
-        }else if(cu==curr::irr){
-           currency= cu;
-            base = sum/3;
-            getcu();
-           return base;
-        }else if(cu==curr::eur){
-           currency= cu;
-            base = sum/2;
-            getcu();
-           return base;
-        }else{
-            
-            throw std::invalid_argument("This currency is not acceptable!!sorry.");
-        }
+float Currency::changetocu(curr cu, float sum) {
+    if (cu == curr::usd) {
+        currency = cu;
+        base = sum;
+        getcu();
+        return base;
+    } else if (cu == curr::irr) {
+        currency = cu;
+        base = sum / 3;
+        getcu();
+        return base;
+    } else if (cu == curr::eur) {
+        currency = cu;
+        base = sum / 2;
+        getcu();
+        return base;
+    } else {
+        throw invalid_argument("This currency is not acceptable!!sorry.");
+    }
 }
 
-void Currency::getcu(){
-         if(currency==curr::usd){
-             std::cout<<base<<" $_usd"<<std::endl;
-          
-        }else if(currency==curr::irr){
-         std::cout<<base<<" IR_irr"<<std::endl;
-          
-        }else if(currency==curr::eur){
-            std::cout<<base<<" €_eur"<<std::endl;
-          
-        }
-}
-int Currency::getbace(){
-          return base;
+void Currency::getcu() {
+    if (currency == curr::usd) {
+        cout << base << " $_usd" << endl;
+    } else if (currency == curr::irr) {
+        cout << base << " IR_irr" << endl;
+    } else if (currency == curr::eur) {
+        cout << base << " €_eur" << endl;
+    }
 }
 
-Eur::Eur(int am=0){
-         among=am;
-         base += among*2;
-} 
-Irr::Irr(int am=0){
-        among=am;
-        base += among*3;
-} 
-Usd::Usd(int am=0){
-        among=am;
-        base += among*1;
 
+Usd::Usd(int am) {
+    among = am;
+    base += among * 1;
+    currency = usd;
 }
-//*************** for test
-//     Usd a(5);
-//     Irr b(2);Eur c(3);
-//   float v=  a.changetocu(curr::irr , a.getbace() + b.getbace() + c.getbace()); all case
-//     cout<<endl<<v; praice they must pay
-    
+
+Usd Usd::operator+(const Usd& other) const {
+    Usd result;
+    result.base = this->base + other.base;
+    return result;
+}
+
+Usd Usd::operator-(const Usd& other) const {
+    Usd result;
+    result.base = this->base - other.base;
+    return result;
+}
+
+
+Eur::Eur(int am) {
+    among = am;
+    base += among * 2;
+    currency = eur;
+}
+
+Eur Eur::operator+(const Eur& other) const {
+    Eur result;
+    result.base = this->base + other.base;
+    return result;
+}
+
+Eur Eur::operator-(const Eur& other) const {
+    Eur result;
+    result.base = this->base - other.base;
+    return result;
+}
+
+
+Irr::Irr(int am) {
+    among = am;
+    base += among * 3;
+    currency = irr;
+}
+
+Irr Irr::operator+(const Irr& other) const {
+    Irr result;
+    result.base = this->base + other.base;
+    return result;
+}
+
+Irr Irr::operator-(const Irr& other) const {
+    Irr result;
+    result.base = this->base - other.base;
+    return result;
+}
+
+
+
+bool Usd::operator==(const Usd& other) const {
+    return std::abs(base - other.base) < 0.0001f; 
+}
+
+bool Usd::operator!=(const Usd& other) const {
+    return !(*this == other);
+}
+
+bool Usd::operator<(const Usd& other) const {
+    return base < other.base;
+}
+
+bool Usd::operator>(const Usd& other) const {
+    return base > other.base;
+}
+
+// عملگرهای مقایسه برای EUR
+bool Eur::operator==(const Eur& other) const {
+    return std::abs(base - other.base) < 0.0001f;
+}
+
+bool Eur::operator!=(const Eur& other) const {
+    return !(*this == other);
+}
+
+bool Eur::operator<(const Eur& other) const {
+    return base < other.base;
+}
+
+bool Eur::operator>(const Eur& other) const {
+    return base > other.base;
+}
+
+// عملگرهای مقایسه برای IRR
+bool Irr::operator==(const Irr& other) const {
+    return std::abs(base - other.base) < 0.0001f;
+}
+
+bool Irr::operator!=(const Irr& other) const {
+    return !(*this == other);
+}
+
+bool Irr::operator<(const Irr& other) const {
+    return base < other.base;
+}
+
+bool Irr::operator>(const Irr& other) const {
+    return base > other.base;
+}
--- CurrencySystem.h
+++ CurrencySystem.h
@@ -1,39 +1,59 @@
-#ifndef  CURRENCYSYSTEM_H  
-#define  CURRENCYSYSTEM_H
+#ifndef CURRENCYSYSTEM_H
+#define CURRENCYSYSTEM_H
 
 #include <stdexcept>
 #include <iostream>
+#include <cmath> // برای تابع abs
 
-enum class curr{usd , irr, eur };
+enum curr { usd, irr, eur, idn };
 
-class Currency{
-    public: 
-       float changetocu(curr cu , float sum);
-       void getcu();
-
-       int getbace();
-       
-    private:
-       curr  currency; 
-             
-    protected:
-    float base=0 , among; 
+class Currency {
+protected:
+    curr currency;
+    float base = 0;
+    float among = 0;
+    
+public:
+    Currency() = default;
+    virtual ~Currency() = default;
+    
+    float changetocu(curr cu, float sum);
+    void getcu();
+    float getbace() const { return base; }
+    curr getCurrencyType() const { return currency; }
 };
 
-class Usd : public Currency{
-    public: 
-         Usd(int am=0);
-
+class Usd : public Currency {
+public:
+    Usd(int am = 0);
+    Usd operator+(const Usd& other) const;
+    Usd operator-(const Usd& other) const;
+    bool operator==(const Usd& other) const;
+    bool operator!=(const Usd& other) const;
+    bool operator<(const Usd& other) const;
+    bool operator>(const Usd& other) const;
 };
 
-class Eur : public Currency{
-    public: 
-         Eur(int am=0);     
-};
-class Irr : public Currency{
-    public: 
-        Irr(int am=0);          
+class Eur : public Currency {
+public:
+    Eur(int am = 0);
+    Eur operator+(const Eur& other) const;
+    Eur operator-(const Eur& other) const;
+    bool operator==(const Eur& other) const;
+    bool operator!=(const Eur& other) const;
+    bool operator<(const Eur& other) const;
+    bool operator>(const Eur& other) const;
 };
 
+class Irr : public Currency {
+public:
+    Irr(int am = 0);
+    Irr operator+(const Irr& other) const;
+    Irr operator-(const Irr& other) const;
+    bool operator==(const Irr& other) const;
+    bool operator!=(const Irr& other) const;
+    bool operator<(const Irr& other) const;
+    bool operator>(const Irr& other) const;
+};
 
 #endif
