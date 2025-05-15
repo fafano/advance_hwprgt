7b3b46bffac593e5b2d53ff48cf2125964f6a94c
fafa <fatemehnourani278@gmail.com>
2025-05-15 17:29:28 +0330

    tro_ghoran

--- shop.cpp
+++ shop.cpp
@@ -1,4 +1,30 @@
 #include "shop.h"
 
-float Shop::balance = 0; 
 
+float Shop::balance=0;
+
+void Shop::withdraw( float amount){
+    balance +=amount;
+}
+
+void cart_Shop::take(Item* it , int among){
+          
+    if(it->get_amount() < among ){
+         throw std::invalid_argument("sorry we have not that mouch!!");
+    }else{
+         items.push_back(it);  
+         price+=(it->get_price());
+     }
+
+}
+float cart_Shop::getprice(){
+    return price;
+}
+
+cart_Shop::~cart_Shop() {
+      
+    for (Item* item : items) {
+        delete item;
+    }
+    items.clear();
+}
--- shop.h
+++ shop.h
@@ -10,9 +10,7 @@ class Shop{
     private:
       static float balance;
     public:
-       static void withdraw( float amount){
-        balance +=amount;
-       }
+       static void withdraw( float amount);
 
 };
 
@@ -21,19 +19,9 @@ class cart_Shop : public Shop{
         std::vector<Item*> items;
         float price=0;
      public:
-       void take(Item* it , int among){
-          
-          if(it->get_amount() < among ){
-              throw std::invalid_argument("sorry we have not that mouch!!");
-          }else{
-              items.push_back(it);  
-              price+=(it->get_price());
-          }
-
-       }
-       float getprice(){
-          return price;
-       }
+       void take(Item* it , int among);
+       float getprice();
+       virtual ~cart_Shop();
 
      
 };
