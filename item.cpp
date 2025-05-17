bddcbeb7e9cc12702e731aee421ed99e0e32ff0f
MinaZarafshan <mina.zarafshan2013@gmail.com>
2025-05-18 00:26:01 +0330

    name has been aded to recite

--- item.cpp
+++ item.cpp
@@ -25,6 +25,9 @@ int Item:: get_amount() const{
 std::string Item :: get_unit() const{
     return unit;
 }
+ void Item:: set_amount(int number){
+    this->amount = number;
+ }
 
 
 //--------------------------------------------------------------------------------------------------------
